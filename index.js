f2ca0008ce4f1abc5d7ee440cf5322b6551443c1
allenmat619 <42273803+allenmat619@users.noreply.github.com>
2020-09-12 11:51:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ const bot = new Discord.Client();
 
 bot.on('message', message => {        
         const str = message.content;
+        
+        if (message.channel.id != '726368915545915413'){
 
-        if ((str.startsWith("_play")==true) || (str.startsWith("-play")==true) || (str.startsWith("!play")==true) || (str.startsWith(";;play")==true)){
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Please play music in the music channel');
-    }
+                if ((str.startsWith("_play")==true) || (str.startsWith("-play")==true) || (str.startsWith("!play")==true) || (str.startsWith(";;play")==true)){
+                        // send back "Pong." to the channel the message was sent in
+                        message.channel.send('Please play music in the music channel');
+                }
+        }
 });
 
 bot.login(process.env.BOT_TOKEN);
